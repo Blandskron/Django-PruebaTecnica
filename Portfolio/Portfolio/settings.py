96cfdcc0f535328ad3433c2fe31f95897c2dd8d3
blandskron <bastianlandskronfreelancer@gmail.com>
2025-01-12 16:29:45 -0300

    üpdate

--- Portfolio/Portfolio/settings.py
+++ Portfolio/Portfolio/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
     'auth_app',
     'contactame',
     'database',
@@ -62,7 +63,7 @@ ROOT_URLCONF = 'Portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Portfolio/contactame/urls.py
+++ Portfolio/contactame/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.contacto_view, name='contacto'),  # Página del formulario
-    path('gracias/', views.gracias_view, name='gracias'),  # Página de agradecimiento
+    path('', views.contacto_view, name='contacto'),
+    path('gracias/', views.gracias_view, name='gracias'),
 ]
